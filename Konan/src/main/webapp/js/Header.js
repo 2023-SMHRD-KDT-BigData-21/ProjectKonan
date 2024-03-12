d1a883f565888b017d00504e00ac14fa77561d05
USER <USER@DESKTOP-MVFTAB2>
2024-03-12 21:57:41 +0900

    0312/이지호/Header.jsp, PostImageController, Qna관련(Content, Form, List), web.xml 건드림

--- Konan/src/main/webapp/js/Header.js
+++ Konan/src/main/webapp/js/Header.js
@@ -1,8 +1,14 @@
 const list = document.querySelectorAll('.list');
 function activeLink() {
+	var currentUrl = window.location.href;
+	console.log(currentUrl);
+	
     list.forEach((item) =>
     item.classList.remove('active_header'));
-    this.classList.add('active_header');
+    
+///    if(currentUrl=="http://localhost:8081/Konan/Main.jsp"){
+//    	this.classList.add('active_header');
+//	}
 }
 
 list.forEach((item) =>
