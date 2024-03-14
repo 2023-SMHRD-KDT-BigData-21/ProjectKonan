41f2f9a3e1734945429474a709ef69e47b7b457c
USER <USER@DESKTOP-MVFTAB2>
2024-03-14 18:15:49 +0900

    0314/06:15/김동현/LogoutController.java, web.xml 로그아웃 구현

--- Konan/src/main/java/com/konan/controller/member/LogoutController.java
+++ Konan/src/main/java/com/konan/controller/member/LogoutController.java
@@ -13,6 +13,6 @@ public class LogoutController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		session.invalidate();
-		response.sendRedirect("main.jsp");
+		response.sendRedirect("Main.jsp");
 	}
 }
