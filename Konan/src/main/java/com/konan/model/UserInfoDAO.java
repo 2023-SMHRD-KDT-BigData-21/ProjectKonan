e79ddc1fdb967b03ae89910cd7cb1e463911c2f9
jiho_ <jiho_@DESKTOP-HMJTN3U>
2024-03-18 11:05:46 +0900

    0318/이지호/프로필사진 오류 수정 PostImageController,UserInfoDAO

--- Konan/src/main/java/com/konan/model/UserInfoDAO.java
+++ Konan/src/main/java/com/konan/model/UserInfoDAO.java
@@ -90,9 +90,10 @@ public class UserInfoDAO {
 
 		String chanu = "C:\\workplace\\git\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\konan\\upload\\";
 		String jiho = "C:\\Users\\USER\\Desktop\\WebServer\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\konan\\upload\\";
+		String jiholg = "C:\\Users\\jiho_\\OneDrive\\Desktop\\Java Server\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\konan\\upload\\";
 		try {
 			if(user.getPropic()!=null) {				
-				File file = new File(jiho + user.getPropic());
+				File file = new File(jiholg + user.getPropic());
 				ImageToBase64 converter = new ImageToBase64();
 				String fileStringValue = converter.convert(file);
 				
